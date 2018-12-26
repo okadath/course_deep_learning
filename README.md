19abf345404b40e2101748936a35b89d755b54ff
okadath <viruz_3107@hotmail.com>
2018-12-26 15:39:51 -0600

    sessions

--- README.md
+++ README.md
@@ -1,28 +1,40 @@
-python3 --version
-pip3 --version
-virtualenv --version
+Deep Learning
+===
+
+Dependencias:
+
+	python3 --version
+	pip3 --version
+	virtualenv --version
 si no estan:
 
-sudo apt update
-sudo apt install python3-dev python3-pip
-sudo pip3 install -U virtualenv  # system-wide install
+	sudo apt update
+	sudo apt install python3-dev python3-pip
+	sudo pip3 install -U virtualenv  # system-wide install
 
 crear nuevo env:
-virtualenv --system-site-packages -p python3 ./venv
 
-activarlo
-source ./venv/bin/activate  # sh, bash, ksh, or zsh
-
-actualizar pip
-pip install --upgrade pip
-
-pip list  # show packages installed within the virtual environment
+	virtualenv --system-site-packages -p python3 ./venv
+	source ./venv/bin/activate  # sh, bash, ksh, or zsh
+	pip install --upgrade pip
+	pip list  # show packages installed within the virtual environment
 
 salir del env :
-deactivate  # don't exit until you're done using TensorFlow
 
-instalar en el ambiente
-pip install --upgrade tensorflow
+	deactivate  # don't exit until you're done using TensorFlow
 
-Recent tensorflow versions require the SoC to include AVX extensions which are not available on Cherry Trail and Apollo Lake based platforms (like UP and UP Squared).
-pip3 install tensorflow==1.5
+instalar en el ambiente:
+
+	pip install --upgrade tensorflow
+	Recent tensorflow versions require the SoC to include AVX extensions
+	pip install tensorflow==1.5
+
+
+Jupyter -> InteractiveSession is	created	it	automatically	setsitself	as	the	default	session,	so	you	don’t	need	a	 with 	block	(but	you	do	need	to	close	the	session
+manually):
+
+	sess	=	tf.InteractiveSession()
+	init.run()
+	result	=	f.eval()
+	print(result)
+	sess.close()
--- test_1.py
+++ test_1.py
@@ -1,10 +1,74 @@
-import	tensorflow	as	tf
-x	=	tf.Variable(3,	name="x")
-y	=	tf.Variable(4,	name="y")
-f	=	x*x*y	+	y	+	2
-sess	=	tf.Session()
-sess.run(x.initializer)
-sess.run(y.initializer)
-result	=	sess.run(f)
-print(result)
-sess.close()
+import tensorflow as tf
+x=tf.Variable(3,name="x")
+y=tf.Variable(4,name="y")
+f=x*x*y+y+2
+#sin sesion
+# sess=tf.Session()
+# sess.run(x.initializer)
+# sess.run(y.initializer)
+# result=sess.run(f)
+# print(result)
+# sess.close()
+
+#con sesion
+with tf.Session()	as sess:
+	x.initializer.run()
+	y.initializer.run()
+	result	=	f.eval()
+	print(result)
+
+
+# Instead of manually running the initializer for every single variable,	you can use the
+# global_variables_initializer() 	function.	Note that it does not actually perform the initialization
+# immediately,	but rather creates a node in the graph that will initialize all variables when it is run:
+init	=	tf.global_variables_initializer()		
+#	prepare an init node
+with tf.Session()	as sess:
+	init.run()		
+	#	actually initialize all the variables
+	result =	f.eval()
+	print(result,"asd")
+
+#cualquier nodo creado es agregado en automatico al grafo x default
+x1	=	tf.Variable(1)
+print(x1.graph is tf.get_default_graph())
+
+#si queremos mantenerlos separados usar bucle:
+
+
+graph	=	tf.Graph()
+with graph.as_default():
+	x2	=	tf.Variable(2)
+
+	print(x2.graph is graph,
+		x2.graph is tf.get_default_graph())
+
+#aunque se repita codigo lo evalua individual
+w	=	tf.constant(3)
+x	=	w	+	2
+y	=	x	+	5
+z	=	x	*	3
+with tf.Session()	as sess:
+	print(y.eval())		#	10
+	print(z.eval())	
+
+#hay que indicarle que los corra juntos ya horre calculos
+with tf.Session()	as sess:
+	y_val,	z_val	=	sess.run([y,	z])
+	print(y_val)		#	10
+	print(z_val)		#	15
+
+#se puede usar como numpy para acceder al GPU
+# import numpy as np
+# from sklearn.datasets import fetch_california_housing
+# housing	=	fetch_california_housing()
+# m,	n	=	housing.data.shape
+# housing_data_plus_bias	=	np.c_[np.ones((m,	1)),	housing.data]
+# X	=	tf.constant(housing_data_plus_bias,	dtype=tf.float32,	name="X")
+# y	=	tf.constant(housing.target.reshape(-1,	1),	dtype=tf.float32,	name="y")
+# XT	=	tf.transpose(X)
+# theta	=	tf.matmul(tf.matmul(tf.matrix_inverse(tf.matmul(XT,	X)),	XT),	y)
+# with tf.Session()	as sess:
+# 				theta_value	=	theta.eval()
+
+#falta hacer el gradiente descendente
